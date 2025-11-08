91102ae6a21d956cb52fa19c7238b5ac820cee4b
kaushlendra pandey <pandeykaushlendra9628@gmail.com>
2025-11-08 16:01:27 +0530

    enw

--- src/main/java/com/warrantyvault/warranty/vault/Repository/WarrantyVaultRepository.java
+++ src/main/java/com/warrantyvault/warranty/vault/Repository/WarrantyVaultRepository.java
@@ -3,12 +3,14 @@ package com.warrantyvault.warranty.vault.Repository;
 import com.warrantyvault.warranty.vault.Entity.WarrantyVault;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface WarrantyVaultRepository extends JpaRepository<WarrantyVault, Long> {
   List<WarrantyVault> findByEmail(String email);
 
